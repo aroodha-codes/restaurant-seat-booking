81f79a24c0a0aee65c019503a105f66edcb30cff
aroodha-codes <aroodha27@gmail.com>
2026-03-09 21:03:57 +0530

    Fix MONGO_URL not defined error

--- backend/server.py
+++ backend/server.py
@@ -15,7 +15,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(MONGO_URL, tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(os.getenv("MONGO_URL"), tlsAllowInvalidCertificates=True)
 db = client[os.environ['DB_NAME']]
 
 app = FastAPI()
