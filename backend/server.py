029a827b4f288a18e7e578a0b8c9dde5b7b86c18
aroodha-codes <aroodha27@gmail.com>
2026-03-10 00:01:10 +0530

    Fix MongoDB TLS version error

--- backend/server.py
+++ backend/server.py
@@ -15,7 +15,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(os.getenv("MONGO_URL"), tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(mongo_url, tlsInsecure=True)
 db = client[os.environ['DB_NAME']]
 
 app = FastAPI()
